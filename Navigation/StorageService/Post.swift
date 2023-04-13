afa0269439998d1f72d8b7daa691e9c69ba1dff1
MadGrizzlyDev <pgbelov89@gmail.com>
2023-04-13 18:05:16 +0300

    task1

---
+++ Navigation/StorageService/Post.swift
@@ -0,0 +1,15 @@
+//
+//  Post.swift
+//  Navigation
+//
+//  Created by Pavel Belov on 21.05.2021.
+//
+
+public struct Post {
+    
+    public let title: String
+    
+    public init(title: String) {
+        self.title = title ;
+    }
+}
